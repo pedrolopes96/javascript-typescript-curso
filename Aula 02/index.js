685dc5fe85fcb4421870768a2ed4d09f5d35311d
Pedro <pedroflopes1710@gmail.com>
2023-05-26 14:38:47 -0300

    Aula02

---
+++ Aula 02/index.js
@@ -0,0 +1,7 @@
+let nome = 'João'; //Criando variáveis
+let sobreNome = 'Lopes' 
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(typeof primeiroNumero); //Descobre o tipo da variável 
