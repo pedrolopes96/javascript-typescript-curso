57f9514d009edbad7892eccdfa7ccb651a0678b6
Pedro <pedroflopes1710@gmail.com>
2023-05-27 11:33:36 -0300

    Aula06

---
+++ Aula06/index.js
@@ -0,0 +1,46 @@
+//Operadores aritméticos
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+
+//Operador de incremento ++
+let contador = 1;
+contador++
+contador++
+contador++
+console.log(contador);
+
+contador = contador + 10;
+console.log(contador);
+
+contador += contador;
+console.log(contador);
+
+//Operador de decremento --
+contador = 1;
+contador--
+contador--
+contador--
+console.log(contador);
+
+contador = contador - 10;
+console.log(contador);
+
+contador -= contador;
+console.log(contador);
+
+//NaN - Not a number
+
+//Convertendo um tipo, parseInt, parseFloat, Number
+let num3 = parseInt(`5`);
+let num4 = (4);
+console.log(num3 + num4);
+console.log(typeof num3);
