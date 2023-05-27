7f7c7efae423db8e04187d8bb7a497f73e531a71
Pedro <pedroflopes1710@gmail.com>
2023-05-27 10:55:16 -0300

    Aula05

---
+++ Aula05/index.js
@@ -0,0 +1,10 @@
+// String, number, undefined, null, boolean, symbol
+const nome = `Pedro`; //String
+const num1 = 10; //Number
+let nomeAluno; //undefined: indica a ausência de um valor atribuído ou indefinido.
+let sobreNome = null; //Indica explicitamente a ausência de um objeto ou valor válido.
+const aprovado = true; // true ou false
+
+
+//Consulta o tipo
+console.log(typeof aprovado);
