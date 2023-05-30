36b748609c7e0ddf12180c20a00a33aaac653436
Pedro <pedroflopes1710@gmail.com>
2023-05-30 15:36:43 -0300

    Aula09

--- Aula09/index.js
+++ Aula09/index.js
@@ -7,4 +7,6 @@ console.log(outraString[4]);
 console.log(outraString.charAt(3));
 console.log(outraString.replace('pedro', 'thais'));
 console.log(outraString.length);
-console.log(outraString.split(' ', 1));
+console.log(outraString.split(' ', 2));
+console.log(outraString.toLocaleUpperCase());
+console.log(outraString.toLowerCase());
