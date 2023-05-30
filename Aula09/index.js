bd1c8aed763b9fe3be2840f5a570b514abd9ed5b
Pedro <pedroflopes1710@gmail.com>
2023-05-29 23:40:23 -0300

    Aula09

--- Aula09/index.js
+++ Aula09/index.js
@@ -4,3 +4,6 @@ console.log(umaString);
 //Posição de um caractere
 let outraString = "pedro lopes";
 console.log(outraString[4]);
+console.log(outraString.charAt(3));
+console.log(outraString.replace('pedro', 'thais'));
+console.log(outraString.length);
