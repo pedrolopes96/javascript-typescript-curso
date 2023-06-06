bfde5c283d665c1eeb481bf70fce78ab342e410e
Pedro <pedroflopes1710@gmail.com>
2023-06-06 14:38:10 -0300

    Aula11

--- Aula11/index.js
+++ Aula11/index.js
@@ -1,5 +1,17 @@
-let num1 = 1;
+let num1 = 25;
 let num2 = 2.5;
 
+console.log(Number.isInteger(num1));
 console.log(num1.toString() + num2);
+
+
+//O número passou a ser uma string
+num1 = num1.toString(2);
+
 console.log(typeof num1);
+
+//Representação binária
+console.log(num1);
+
+//Condição booleana de verificação
+console.log(Number.isInteger(num1));
