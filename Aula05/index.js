61648be87d1d63a89e3a0946e81e8cd5f5b86478
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-07-04 14:23:39 -0300

    Aula05

--- Aula05/index.js
+++ Aula05/index.js
@@ -3,8 +3,8 @@ const nome = `Pedro`; //String
 const num1 = 10; //Number
 let nomeAluno; //undefined: indica a ausência de um valor atribuído ou indefinido.
 let sobreNome = null; //Indica explicitamente a ausência de um objeto ou valor válido.
-const boolean = true; // true ou false
+const aprovado = true; // true ou false
 
 
 //Consulta o tipo
-console.log(typeof aprovado);
+console.log(typeof aprovado, aprovado);
