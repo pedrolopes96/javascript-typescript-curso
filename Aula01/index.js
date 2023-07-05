edb0ba6b0414cab2c1faeb2bdebb8e53b9bf7cd1
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-07-05 16:33:26 -0300

    Aula 01

--- Aula01/index.js
+++ Aula01/index.js
@@ -4,6 +4,8 @@ console.log('Pedro Fernandes');
 console.log(`Pedro Fernandes Lopes`);
 
 //Template Strings
-
 let idade = 10;
 console.log(`Nome: ${idade}`);
+
+//Literais
+console.log(1050, 50.10);
