539aa68a298b7c07c6e0c5d9ab492f1e8685b112
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-07-06 15:37:35 -0300

    Aula02

--- Aula02/index.js
+++ Aula02/index.js
@@ -1,9 +1,3 @@
-let nome = 'João'; //Criando variáveis
-let sobreNome = 'Lopes' 
-
-const primeiroNumero = 5;
-const segundoNumero = 10;
-
-console.log(typeof primeiroNumero); //Descobre o tipo da variável 
+ 
 
 alert("Olá, Pedro");
