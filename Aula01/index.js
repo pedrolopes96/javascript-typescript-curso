3eda4c7cda5e12965e7084606cd0a91f60d61ba8
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-07-09 15:53:15 -0300

    Aula01

--- Aula01/index.js
+++ Aula01/index.js
@@ -7,10 +7,6 @@ console.log(`Pedro Fernandes Lopes`);
 let idade = 26;
 console.log(`Idade: ${idade}`);
 
-//Literais
-console.log(1050, 50.10);
 
-//Teste da aula
-console.log('Meu nome é "Pedro Lopes". Estou aprendendo JavaScript às', 15, 'horas da tarde.');
 
 
